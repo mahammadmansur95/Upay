1f62d98620b56c91d3afdacc148eef966b5a4d5e
mahammadmansur95 <mansuralimahammad@gmail.com>
2021-09-09 11:25:45 +0530

    added component how to use app

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import howtoUseApp from '../API/howToUse';
 
 const AboutUs = () => {
+  const [aboutData, setAboutData] = useState(howtoUseApp);
+
   return (
     <>
       <section className="common_section our_services">
         <div className="container mb-5">
           <div className="row">
-                <div className="col-12 col-lg-5 text-center our_service_leftside_img">
-                    <img src="./images/hero3.jpg" alt="aboutUsImg" />
-                </div>
-                <div className="col-12 col-lg-7 our_services_list">
-                    <h1 className="main-heading">How to Use the APP</h1>
+            <div className="col-12 col-lg-5 text-center our_service_leftside_img">
+              <img src="./images/hero3.jpg" alt="aboutUsImg" />
+            </div>
+            <div className="col-12 col-lg-7 our_services_list">
+              <h1 className="main-heading">How to Use the APP</h1>
+              {aboutData.map((curEle) => {
+                const { id, title, info } = curEle;
+                return (
+                  <>
                     <div className="row our_services_info">
-                        <div className="col-1 our_services_number">1</div>
-                        <div className="col-10 our_services_data">
-                            <h2>Sign in</h2>
-                            <p className="main_hero_para">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                Etquia quis?Lorem ipsum
-                            </p>
-                        </div>
+                      <div className="col-1 our_services_number">{id}</div>
+                      <div className="col-10 our_services_data">
+                        <h2>{title}</h2>
+                        <p className="main_hero_para">{info}</p>
+                      </div>
                     </div>
-                </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </section>
